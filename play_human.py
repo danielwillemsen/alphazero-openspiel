5b3934dcd0c960cbd1208b30dcfe78c147c82ef5
Daniel Willemsen <danielwillemsen@live.nl>
2019-09-04 07:35:33 +0200

    added example net

--- play_human.py
+++ play_human.py
@@ -4,7 +4,7 @@ from mctsagent import MCTSAgent
 
 if __name__ == '__main__':
 	net = Net()
-	net.load_state_dict(torch.load("models/initial105.pth", map_location='cpu'))
+	net.load_state_dict(torch.load("models/example_model.pth", map_location='cpu'))
 	net.eval()
 	mctsagent = MCTSAgent(net.predict)
 	mctsagent.play_game_vs_human_net_only()
