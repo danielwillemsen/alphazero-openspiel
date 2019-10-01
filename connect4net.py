86987425ac8f85c2d0c3720cfad32f09a001df6a
danielwillemsen <danielwillemsen@live.nl>
2019-10-01 08:21:14 +0200

    resettest

--- connect4net.py
+++ connect4net.py
@@ -35,6 +35,65 @@ class Net(nn.Module):
         self.device = kwargs.get('device', torch.device('cpu'))
         self.time1 = time.time()
 
+        self.resblock1 = ResidualBlock(self.num_states, 50)
+        self.resblock2 = ResidualBlock(50, 50)
+        self.resblock3 = ResidualBlock(50, 50)
+        self.resblock4 = ResidualBlock(50, 50)
+        self.resblock5 = ResidualBlock(50, 50)
+        self.fc1 = nn.Linear(50*self.width*self.height, self.num_distinct_actions + 1)
+        return
+
+    def forward(self, x):
+        """
+
+        @param x: input tensor
+        @return: Policy tensor and value tensor
+        """
+        x = self.resblock1(x)
+        x = self.resblock2(x)
+        x = self.resblock3(x)
+        x = self.resblock4(x)
+        x = self.resblock5(x)
+
+        x = x.view(-1, self.height * self.width * 50)
+        x = self.fc1(x)
+        xp, v = x.split(self.num_distinct_actions, 1)
+        return F.softmax(xp, dim=1), torch.tanh(v)
+
+    def predict(self, state):
+        """
+
+        @param state: state to predict next move for
+        @return: List of policy and the value
+        """
+        board = state_to_board(state, self.state_shape)
+
+        with torch.no_grad():
+            tens = torch.from_numpy(board).float().to(self.device)
+            tens = tens.unsqueeze(0)
+            p_t, v_t = self.forward(tens)
+            ps = p_t.tolist()[0]
+            v = float(v_t)
+        return ps, v
+
+
+class NetOld(nn.Module):
+    """Neural network for connect_four.
+    The input representation is a ([empty locations, own pieces, opponent pieces] x width x height) matrix.
+    The output is a vector of size width, for the policy and an additional scalar for the value.
+    """
+
+    def __init__(self, state_shape, num_distinct_actions, **kwargs):
+        super(NetOld, self).__init__()
+        self.state_shape = state_shape
+        self.num_states = state_shape[0]
+        self.height = state_shape[1]
+        self.width = state_shape[2]
+        self.num_distinct_actions = num_distinct_actions
+
+        self.device = kwargs.get('device', torch.device('cpu'))
+        self.time1 = time.time()
+
         self.conv1 = nn.Conv2d(self.num_states, 50, 3, padding=1)
         self.bn1 = nn.BatchNorm2d(50)
         self.conv2 = nn.Conv2d(50, 50, 3, padding=1)
@@ -86,3 +145,27 @@ class Net(nn.Module):
             ps = p_t.tolist()[0]
             v = float(v_t)
         return ps, v
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.bn1 = nn.BatchNorm2d(in_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.conv1 = nn.Conv2d(in_channels, out_channels, 3, padding=1)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, 3, padding=1)
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.use_1x1conv = (in_channels != out_channels)
+
+        if self.use_1x1conv:
+            self.conv3 = nn.Conv2d(in_channels, out_channels, 1, padding=0)
+
+    def forward(self, x):
+        x2 = self.conv1(F.leaky_relu(self.bn1(x)))
+        x2 = self.conv2(F.leaky_relu(self.bn2(x2)))
+        if self.use_1x1conv:
+            x = self.conv3(x)
+        return x + x2
