e342993b95f1bf0799d5a3acb0f811aac92ae213
danielwillemsen <danielwillemsen@live.nl>
2020-05-09 17:12:40 +0200

    Cleaned connect4net

--- connect4net.py
+++ connect4net.py
@@ -34,23 +34,15 @@ class Net(nn.Module):
 
         self.device = kwargs.get('device', torch.device('cpu'))
         self.time1 = time.time()
-        self.n_filts = 100
+        self.n_filts = 50
+
         self.resblock1 = ResidualBlock(self.num_filters_input, self.n_filts)
         self.resblock2 = ResidualBlock(self.n_filts, self.n_filts)
         self.resblock3 = ResidualBlock(self.n_filts, self.n_filts)
         self.resblock4 = ResidualBlock(self.n_filts, self.n_filts)
         self.resblock5 = ResidualBlock(self.n_filts, self.n_filts)
-        #self.resblock6 = ResidualBlock(50, 50)
-        #self.resblock7 = ResidualBlock(50, 50)
-        #self.resblock8 = ResidualBlock(50, 50)
-        #self.resblock9 = ResidualBlock(50, 50)
-        #self.resblock10 = ResidualBlock(50, 50)
-        self.fc1v = nn.Linear(self.n_filts*self.width*self.height, 100)
-        self.fc1p = nn.Linear(self.n_filts*self.width*self.height, 100)
 
-        self.fc2v = nn.Linear(100, 1)
-        self.fc2p = nn.Linear(100, self.num_distinct_actions)
-        # self.fc1 = nn.Linear(50*self.width*self.height, self.num_distinct_actions + 1)
+        self.fc1 = nn.Linear(self.n_filts*self.width*self.height, self.num_distinct_actions + 1)
         return
 
     def forward(self, x):
@@ -64,106 +56,12 @@ class Net(nn.Module):
         x = self.resblock3(x)
         x = self.resblock4(x)
         x = self.resblock5(x)
-        #x = self.resblock6(x)
-        #x = self.resblock7(x)
-        #x = self.resblock8(x)
-        #x = self.resblock9(x)
-        #x = self.resblock10(x)
 
         x = x.view(-1, self.height * self.width * self.n_filts)
+        x = self.fc1(x)
+        xp, v = x.split(self.num_distinct_actions, 1)
 
-        xv = F.leaky_relu(self.fc1v(x))
-        xp = F.leaky_relu(self.fc1p(x))
-        # x = F.leaky_relu(self.fc1_bn(self.fc1(x)))
-        # x = elf.fc2(x)
-        # xp, v = x.split(self.num_distinct_actions, 1)
-        #return F.softmax(xp, dim=1), torch.tanh(v)
-        return F.softmax(self.fc2p(xp), dim=1), torch.tanh(self.fc2v(xv))
-
-        # x = x.view(-1, self.height * self.width * 50)
-        # x = self.fc1(x)
-        # xp, v = x.split(self.num_distinct_actions, 1)
-        # return F.softmax(xp, dim=1), torch.tanh(v)
-
-    def predict(self, state):
-        """
-
-        @param state: state to predict next move for
-        @return: List of policy and the value
-        """
-        board = state_to_board(state, self.state_shape)
-
-        with torch.no_grad():
-            tens = torch.from_numpy(board).float().to(self.device)
-            tens = tens.unsqueeze(0)
-            p_t, v_t = self.forward(tens)
-            ps = p_t.tolist()[0]
-            v = float(v_t)
-        return ps, v
-
-
-class NetOld(nn.Module):
-    """Neural network for connect_four.
-    The input representation is a ([empty locations, own pieces, opponent pieces] x width x height) matrix.
-    The output is a vector of size width, for the policy and an additional scalar for the value.
-    """
-
-    def __init__(self, state_shape, num_distinct_actions, **kwargs):
-        super(NetOld, self).__init__()
-        self.state_shape = state_shape
-        self.num_states = state_shape[0]
-        self.height = state_shape[1]
-        self.width = state_shape[2]
-        self.num_distinct_actions = num_distinct_actions
-
-        self.device = kwargs.get('device', torch.device('cpu'))
-        self.time1 = time.time()
-
-        self.conv1 = nn.Conv2d(self.num_states + 1, 50, 3, padding=1)
-        self.bn1 = nn.BatchNorm2d(50)
-        self.conv2 = nn.Conv2d(50, 50, 3, padding=1)
-        self.bn2 = nn.BatchNorm2d(50)
-        self.conv3 = nn.Conv2d(50, 50, 3, padding=1)
-        self.bn3 = nn.BatchNorm2d(50)
-        self.conv4 = nn.Conv2d(50, 50, 3, padding=1)
-        self.bn4 = nn.BatchNorm2d(50)
-        # self.conv5 = nn.Conv2d(50, 50, (4, 4), padding=1)
-        # self.bn5 = nn.BatchNorm2d(50)
-        # self.conv6 = nn.Conv2d(50, 50, (4, 4), padding=1)
-        # self.bn6 = nn.BatchNorm2d(50)
-        # self.fc1 = nn.Linear(self.height * self.width * 50, 50)
-        # self.fc1_bn = nn.BatchNorm1d(50)
-        # self.fc2 = nn.Linear(50, self.num_distinct_actions + 1)
-        self.fc1v = nn.Linear(50*self.width*self.height, 100)
-        self.fc1p = nn.Linear(50*self.width*self.height, 100)
-
-        self.fc2v = nn.Linear(100, 1)
-        self.fc2p = nn.Linear(100, self.num_distinct_actions)
-        return
-
-    def forward(self, x):
-        """
-
-        @param x: input tensor
-        @return: Policy tensor and value tensor
-        """
-        x = F.leaky_relu(self.bn1(self.conv1(x)))
-        x = F.leaky_relu(self.bn2(self.conv2(x)))
-        x = F.leaky_relu(self.bn3(self.conv3(x)))
-        x = F.leaky_relu(self.bn4(self.conv4(x)))
-        # x = F.leaky_relu(self.bn5(self.conv5(x)))
-        # x = F.leaky_relu(self.bn6(self.conv6(x)))
-
-        x = x.view(-1, (self.height) * (self.width) * 50)
-        x = x.view(-1, self.height * self.width * 50)
-
-        xv = F.leaky_relu(self.fc1v(x))
-        xp = F.leaky_relu(self.fc1p(x))
-        # x = F.leaky_relu(self.fc1_bn(self.fc1(x)))
-        # x = self.fc2(x)
-        # xp, v = x.split(self.num_distinct_actions, 1)
-        #return F.softmax(xp, dim=1), torch.tanh(v)
-        return F.softmax(self.fc2p(xp), dim=1), torch.tanh(self.fc2v(xv))
+        return F.softmax(xp, dim=1), torch.tanh(v)
 
     def predict(self, state):
         """
