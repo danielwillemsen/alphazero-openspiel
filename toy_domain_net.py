ba03a1895b07e9e7aa08cae84167d7ef9a3d35ae
danielwillemsen <danielwillemsen@live.nl>
2020-08-02 14:48:42 +0200

    Added random initialization for toy domain

--- toy_domain_net.py
+++ toy_domain_net.py
@@ -94,10 +94,11 @@ def update_pvtables_tictactoe(example, pvtables):
 
 backup_types = ["on-policy", "soft-Z", "A0C", "off-policy"]#, "greedy-forward", "greedy-forward-N"]
 
+game = pyspiel.load_game("connect_four")
 length = 5
-n_games = 1000
+n_games = 2500
 num_distinct_actions = 4
-pvtables = {backup_type: PVTable_tictactoe(length, use_random=False) for backup_type in backup_types}
+pvtables = {backup_type: PVTable_tictactoe(length, use_random=True) for backup_type in backup_types}
 alpha = 0.025
 backup_res = {backup_type: [] for backup_type in backup_types}
 
@@ -113,15 +114,17 @@ for i_game in range(n_games):
                               backup="off-policy",
                               backup_types=backup_types,
                               length=length,
-                              initial_sequence=[0, 4, 2])
+                              initial_sequence=[])# 0 4 2
     for example in examples:
         update_pvtables_tictactoe(example, pvtables)
 
         #For further data visualization
         for key, pvtable in pvtables.items():
             #backup_res[key].append(np.copy(pvtable.values))
-            backup_res[key].append(np.copy(pvtable.values["0 4 2"]))
+            backup_res[key].append(np.copy(pvtable.values[""]))
 
+    # if backup_res["off-policy"][-1][3,0] > 0.0001:
+    #     a=2
     if i_game%2 == 0:
         print("Game_no:", i_game, "/", n_games)
 
@@ -137,33 +140,33 @@ for key, val in backup_res.items():
 #    plt.plot(val[:, 3, 0], label=key)
 plt.legend()
 plt.show()
-#
-# labels = [type_name.title() for type_name in backup_types]
-# cmap = cm.get_cmap('RdYlGn', 30)
-#
-# i = 0
-# fig, axes = plt.subplots(nrows=len(backup_types), ncols=1, figsize=(8,8), tight_layout=False, constrained_layout=True)
-# axes[0].set_title(f"Progress over {n_games} games")
-#
-# fig.set_tight_layout(False)
-# for ax in axes.flat:
-#     #ax.set_axis_off()
-# #     im = ax.imshow(np.expand_dims(values_total[i],0), label=labels[i], cmap=cmap, vmin=-0.1, vmax=0.1)
-#     im = ax.imshow(backup_res[backup_types[i]][:,:,0], label=backup_types[i], cmap=cmap, vmin=-0.1, vmax=0.1, aspect='auto', interpolation='none')
-#     ax.set_ylabel(labels[i])
-#     ax.get_yaxis().set_ticks([])
-#     ax.set_xticks(np.arange(length))
-#     ax.set_xticklabels([str(i) for i in range(length)])
-#     i+=1
-#
-# axes[-1].annotate("State", xy=(0.5, -0.3), xycoords=axes[-1].get_window_extent,
-#                   xytext=(0,0), textcoords="offset points", ha='center', va='bottom')
-#
-# #fig.subplots_adjust(bottom=0.1, top=0.9, left=0.0, right=0.7,
-# #                    wspace=0.02, hspace=0.02)
-#
-# #cb_ax = fig.add_axes([0.87, 0.1, 0.02, 0.8])
-# cbar = fig.colorbar(im, ax=axes)
-# cbar.set_label('Value', rotation=270, labelpad=10.5)
-#
-# plt.show()
+
+labels = [type_name.title() for type_name in backup_types]
+cmap = cm.get_cmap('RdYlGn', 30)
+
+i = 0
+fig, axes = plt.subplots(nrows=len(backup_types), ncols=1, figsize=(8,8), tight_layout=False, constrained_layout=True)
+axes[0].set_title(f"Progress over {n_games} games")
+
+fig.set_tight_layout(False)
+for ax in axes.flat:
+    #ax.set_axis_off()
+#     im = ax.imshow(np.expand_dims(values_total[i],0), label=labels[i], cmap=cmap, vmin=-0.1, vmax=0.1)
+    im = ax.imshow(backup_res[backup_types[i]][:,:,0], label=backup_types[i], cmap=cmap, vmin=-0.1, vmax=0.1, aspect='auto', interpolation='none')
+    ax.set_ylabel(labels[i])
+    ax.get_yaxis().set_ticks([])
+    ax.set_xticks(np.arange(length))
+    ax.set_xticklabels([str(i) for i in range(length)])
+    i+=1
+
+axes[-1].annotate("State", xy=(0.5, -0.3), xycoords=axes[-1].get_window_extent,
+                  xytext=(0,0), textcoords="offset points", ha='center', va='bottom')
+
+#fig.subplots_adjust(bottom=0.1, top=0.9, left=0.0, right=0.7,
+#                    wspace=0.02, hspace=0.02)
+
+#cb_ax = fig.add_axes([0.87, 0.1, 0.02, 0.8])
+cbar = fig.colorbar(im, ax=axes)
+cbar.set_label('Value', rotation=270, labelpad=10.5)
+
+plt.show()
